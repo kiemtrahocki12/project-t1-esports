ec8cbf3bb57077108acd8614cf913c38c596eaa6
kiemtrahocki12 <1909014@lsts.edu.vn>
2025-12-01 15:23:54 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (x.length === 0) return;
+  
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 10000); 
+}
